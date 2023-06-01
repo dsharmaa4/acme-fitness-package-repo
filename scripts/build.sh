e1b63fff6a603306634ea603700cc86ba75b9c3c
doddatpivotal <dpfeffer@vmware.com>
2023-05-31 22:14:39 -0400

    Adds verison 1.0.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,13 @@ kbld -f packages/acme-fitness/1.0.0/bundle/config --imgpkg-lock-output packages/
 ytt -f packages/acme-fitness/1.0.0/package-template.yaml \
     --data-value-file openapi=<(ytt -f packages/acme-fitness/1.0.0/bundle/config/values-schema.yaml --data-values-schema-inspect -o openapi-v3) \
     -v version="1.0.0" > package-repo/packages/acme-fitness.external.demo-dependencies.learn/1.0.0.yaml
+
+kbld -f packages/acme-fitness/1.0.1/bundle/config --imgpkg-lock-output packages/acme-fitness/1.0.1/bundle/.imgpkg/images.yml
+# Generate package file from from template
+ytt -f packages/acme-fitness/1.0.1/package-template.yaml \
+    --data-value-file openapi=<(ytt -f packages/acme-fitness/1.0.1/bundle/config/values-schema.yaml --data-values-schema-inspect -o openapi-v3) \
+    -v version="1.0.1" > package-repo/packages/acme-fitness.external.demo-dependencies.learn/1.0.1.yaml
+
 # Copy over metada file
 cp packages/acme-fitness/metadata.yaml package-repo/packages/acme-fitness.external.demo-dependencies.learn/
 
